4120f47414b0ed2cda90c7a50f8c3478fda29e0b
Nat Welch <nat@natwelch.com>
2015-04-14 16:36:38 +0000

    set up charecter movement.

--- public/asteroids.js
+++ public/asteroids.js
@@ -1,23 +1,40 @@
 // Hyperspace is Asteroids in Javascript
+// Authors: @kenpratt and @icco
 //
-// Current code is based off of the examples from
+// This code is dependent on the coquette library:
 // http://coquette.maryrosecook.com/
+
+// The main game initializer. This function sets up the game.
 var Hyperspace = function() {
   this.c = new Coquette(this, "canvas", 1000, 600, "#000");
 
   this.connect();
 
-  this.c.entities.create(Person, { center: { x:250, y:40 }, color:"#099" });
-
-  this.c.entities.create(Person, { center: { x:256, y:110 }, color:"#f07",
+  // Create the ship that the current player drives. It differs from all other
+  // ships in that it has an update loop (called every tick) that takes in
+  // directions from the keyboard.
+  this.c.entities.create(Ship, { center: { x:256, y:110 }, color:"#f07",
     update: function() {
+      // TODO(icco): Support W and ,
       if (this.c.inputter.isDown(this.c.inputter.UP_ARROW)) {
         this.center.y -= 0.4;
       }
+
+      // TODO(icco): Support S and O
+      if (this.c.inputter.isDown(this.c.inputter.DOWN_ARROW)) {
+        this.center.y += 0.4;
+      }
+
+      // TODO(icco): Support D and E
+      if (this.c.inputter.isDown(this.c.inputter.RIGHT_ARROW)) {
+        this.center.x += 0.4;
+      }
+
+      // TODO(icco): Support A
+      if (this.c.inputter.isDown(this.c.inputter.LEFT_ARROW)) {
+        this.center.x -= 0.4;
+      }
     },
-    collision: function(other) {
-      other.center.y = this.center.y; // follow the player
-    }
   });
 };
 
@@ -39,7 +56,9 @@ Hyperspace.prototype.connect = function() {
   };
 }
 
-var Person = function(game, settings) {
+// The actual ship entity. One of these will be created for every single player
+// in the game. Please set the color.
+var Ship = function(game, settings) {
   this.c = game.c;
   for (var i in settings) {
     this[i] = settings[i];
@@ -57,5 +76,6 @@ var Person = function(game, settings) {
 };
 
 window.addEventListener('load', function() {
+  // Begin the game once the page is loaded! Party like it's 1979!
   new Hyperspace();
 });
