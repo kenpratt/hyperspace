f511a8bb676cdfc7da06c99fa4cdfefea39aac4f
Nat Welch <nat@natwelch.com>
2015-04-14 19:31:06 +0000

    allow for turning.

--- public/asteroids.js
+++ public/asteroids.js
@@ -14,25 +14,46 @@ var Hyperspace = function() {
   // ships in that it has an update loop (called every tick) that takes in
   // directions from the keyboard.
   this.c.entities.create(Ship, { center: { x:256, y:110 }, color:"#f07",
+    // Movement is based off of this SO article which basically reminded me how
+    // vectors work: http://stackoverflow.com/a/3639025/1063
     update: function() {
+      var angleToVector = function(angle) {
+        // Convert to radians.
+        var r = angle * 0.01745;
+        return unitVector({ x: Math.sin(r), y: -Math.cos(r) });
+      };
+      var magnitude = function(vector) {
+        return Math.sqrt(vector.x * vector.x + vector.y * vector.y);
+      };
+      var unitVector = function(vector) {
+        return {
+          x: vector.x / magnitude(vector),
+          y: vector.y / magnitude(vector)
+        };
+      };
+
+      this.angle %= 360;
+
+      // Back and forth movement
       // TODO(icco): Support W and ,
       if (this.c.inputter.isDown(this.c.inputter.UP_ARROW)) {
-        this.center.y -= 0.4;
-      }
+        var vector = angleToVector(this.angle);
+        this.center.x += vector.x;
+        this.center.y += vector.y;
 
       // TODO(icco): Support S and O
-      if (this.c.inputter.isDown(this.c.inputter.DOWN_ARROW)) {
-        this.center.y += 0.4;
+      } else if (this.c.inputter.isDown(this.c.inputter.DOWN_ARROW)) {
+        // TODO(icco): Support breaking.
       }
 
+      // Turning.
       // TODO(icco): Support D and E
       if (this.c.inputter.isDown(this.c.inputter.RIGHT_ARROW)) {
-        this.center.x += 0.4;
-      }
+        this.angle += 0.6;
 
       // TODO(icco): Support A
-      if (this.c.inputter.isDown(this.c.inputter.LEFT_ARROW)) {
-        this.center.x -= 0.4;
+      } else if (this.c.inputter.isDown(this.c.inputter.LEFT_ARROW)) {
+        this.angle -= 0.6;
       }
     },
   });
@@ -59,11 +80,11 @@ Hyperspace.prototype.connect = function() {
 // This defines the basic ship shape as a series of verices for a path to
 // follow.
 var ship_shape = [
-  [0, 0],
-  [-5, 10],
-  [0, 7],
-  [5, 10],
-  [0, 0]
+  [ 0, -5],
+  [-5,  5],
+  [ 0,  2],
+  [ 5,  5],
+  [ 0, -5]
 ]
 
 // The actual ship entity. One of these will be created for every single player
@@ -76,6 +97,8 @@ var Ship = function(game, settings) {
 
   // This is the size of the ship.
   this.scale = 1.5;
+  this.size = { x: 10 * this.scale, y: 10 * this.scale }
+  this.angle = 0;
 
   // This is run every tick to draw the ship.
   this.draw = function(ctx) {
