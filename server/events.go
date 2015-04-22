7941494d673cca680c716e88cd78b8112f9238ff
Ken Pratt <ken@kenpratt.net>
2015-04-22 16:00:23 -0400

    Changed event fields to private, added Time() function.

--- server/events.go
+++ server/events.go
@@ -1,104 +1,133 @@
 package main
 
 type GameEvent interface {
+	Time() uint64
 	Execute(*GameState) error
 }
 
 // create a ship
 type CreateShipEvent struct {
-	Time     uint64
-	Id       string
-	Position *Point
+	time     uint64
+	id       string
+	position *Point
+}
+
+func (e *CreateShipEvent) Time() uint64 {
+	return e.time
 }
 
 func (e *CreateShipEvent) Execute(state *GameState) error {
-	state.Ships[e.Id] = CreateShip(e.Id, e.Position)
+	state.Ships[e.id] = CreateShip(e.id, e.position)
 	return nil
 }
 
 // create an asteroid
 type CreateAsteroidEvent struct {
-	Time     uint64
-	Id       string
-	Position *Point
-	Angle    float64
-	Velocity *Vector
-	Shape    []*Point
+	time     uint64
+	id       string
+	position *Point
+	angle    float64
+	velocity *Vector
+	shape    []*Point
+}
+
+func (e *CreateAsteroidEvent) Time() uint64 {
+	return e.time
 }
 
 func (e *CreateAsteroidEvent) Execute(state *GameState) error {
-	state.Asteroids[e.Id] = CreateAsteroid(e.Id, e.Position, e.Angle, e.Velocity, e.Shape)
+	state.Asteroids[e.id] = CreateAsteroid(e.id, e.position, e.angle, e.velocity, e.shape)
 	return nil
 }
 
 // remove a ship
 type RemoveShipEvent struct {
-	Time   uint64
-	ShipId string
+	time   uint64
+	shipId string
+}
+
+func (e *RemoveShipEvent) Time() uint64 {
+	return e.time
 }
 
 func (e *RemoveShipEvent) Execute(state *GameState) error {
-	delete(state.Ships, e.ShipId)
+	delete(state.Ships, e.shipId)
 	return nil
 }
 
 // change ship acceleration direction
 type ChangeAccelerationEvent struct {
-	Time      uint64
-	ShipId    string
-	Direction int8
+	time      uint64
+	shipId    string
+	direction int8
+}
+
+func (e *ChangeAccelerationEvent) Time() uint64 {
+	return e.time
 }
 
 func (e *ChangeAccelerationEvent) Execute(state *GameState) error {
-	s := state.Ships[e.ShipId]
+	s := state.Ships[e.shipId]
 	if s == nil {
 		return GameError{"Ship doesn't exist for player"}
 	}
 
-	s.Acceleration = e.Direction
+	s.Acceleration = e.direction
 	return nil
 }
 
 // change ship rotation direction
 type ChangeRotationEvent struct {
-	Time      uint64
-	ShipId    string
-	Direction int8
+	time      uint64
+	shipId    string
+	direction int8
+}
+
+func (e *ChangeRotationEvent) Time() uint64 {
+	return e.time
 }
 
 func (e *ChangeRotationEvent) Execute(state *GameState) error {
-	s := state.Ships[e.ShipId]
+	s := state.Ships[e.shipId]
 	if s == nil {
 		return GameError{"Ship doesn't exist for player"}
 	}
 
-	s.Rotation = e.Direction
+	s.Rotation = e.direction
 	return nil
 }
 
 // fire ship laser!
 type FireEvent struct {
-	Time         uint64
-	ShipId       string
-	ProjectileId string
+	time         uint64
+	shipId       string
+	projectileId string
 	Created      uint64
 }
 
+func (e *FireEvent) Time() uint64 {
+	return e.time
+}
+
 func (e *FireEvent) Execute(state *GameState) error {
-	s := state.Ships[e.ShipId]
+	s := state.Ships[e.shipId]
 	if s == nil {
 		return GameError{"Ship doesn't exist for player"}
 	}
 
 	pos := *s.Position // Clone ship position
-	projectile := CreateProjectile(e.ProjectileId, &pos, s.Angle, e.Created, e.ShipId)
+	projectile := CreateProjectile(e.projectileId, &pos, s.Angle, e.Created, e.shipId)
 	state.Projectiles[projectile.Id] = projectile
 	return nil
 }
 
 // remove dead objects
 type CleanupEvent struct {
-	Time uint64
+	time uint64
+}
+
+func (e *CleanupEvent) Time() uint64 {
+	return e.time
 }
 
 func (e *CleanupEvent) Execute(state *GameState) error {
